3c8cbfcd93acd9e227a5855166936487c4eacd54
Diogo Assis <diogogamegame@gmail.com>
2025-10-02 20:56:14 -0300

    Criado a entidade: Especialidades com atributos Id  e  String nome

---
+++ src/main/java/com/example/demo/entities/Especialidades.java
@@ -0,0 +1,33 @@
+
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "especialidades")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Especialidades {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_especialidade")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
+
+
